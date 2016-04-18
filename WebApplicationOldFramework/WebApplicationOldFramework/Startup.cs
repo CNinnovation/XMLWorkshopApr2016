f268ec6f83b0aea0a44472e82596a81dbe2f16d7
Christian Nagel <christian@christiannagel.com>
2016-04-18 11:17:29 +0200

    add XAML and Web app samples

---
+++ WebApplicationOldFramework/WebApplicationOldFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationOldFramework.Startup))]
+namespace WebApplicationOldFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
